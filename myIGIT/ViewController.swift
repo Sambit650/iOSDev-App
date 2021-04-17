7edad74261f9e342f7464e63b10a3e752501255e
Sambit650 <sambit650@gmail.com>
2021-04-17 22:32:24 +0530

    Added webView

--- myIGIT/ViewController.swift
+++ myIGIT/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadWebView()
     }
-
-
+    
+    private func loadWebView() {
+        let url = URL(string: "https://myigit.info")
+        let requestObj = URLRequest(url: url! as URL)
+        webView.load(requestObj)
+    }
 }
 
