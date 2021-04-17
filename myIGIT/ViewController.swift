e003273005f2fad0832066982a467df1d7cc6437
Sambit Das <sambit.das@zopsmart.com>
2021-04-17 21:52:02 +0530

    Initial Commit

---
+++ myIGIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myIGIT
+//
+//  Created by Sambit Das on 27/01/1943 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
